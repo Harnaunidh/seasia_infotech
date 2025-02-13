524c614a84f6e5ab0cba76d539b1acd595c2d097
singhharnaunidh <singhharnaunidh@seasiaconnect.com>
2025-02-13 17:44:50 +0530

    modules

---
+++ mymodule.py
@@ -0,0 +1,21 @@
+import math
+
+
+def area_of_rectangle(a, b):
+    area = a*b
+    return area
+
+
+def area_of_circle(r):
+    area = math.pi*r**2
+    return area
+
+
+def area_of_triangle(a, b):
+    area = 0.5*a*b
+    return area
+
+
+def area_of_square(a):
+    area = a**2
+    return area
