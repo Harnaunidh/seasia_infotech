d8a1713ae11d9ef0eddd70590c7862bdd6848cd6
Harnaunidh <singhharnaunidh@seasiaconnect.com>
2025-03-06 10:12:20 +0530

    rename

--- django_project_1/todo/forms.py
+++ django_project_1/todo/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()  # Adding an email field
 
     class Meta:
         model = User
